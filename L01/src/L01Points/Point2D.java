edc6ad502c5db827965523a497ca1746dc42aec7
Dmitry <dmitry.wu@gmail.com>
2024-09-03 10:59:17 +0300

    Lecture L01, v.1 Point2D simple ctor, override

---
+++ L01/src/L01Points/Point2D.java
@@ -0,0 +1,29 @@
+package L01Points;
+
+public class Point2D {
+    int x, y;
+
+    public Point2D(int valueX, int valueY) {
+        x = valueX;
+        y = valueY;
+    }
+
+    public Point2D() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point2D(int value) {
+        x = value;
+        y = value;
+    }
+
+    private String getInfo() {
+        return String.format("x: %d; y: %d", x, y);
+    }
+
+    @Override
+    public String toString() {
+        return getInfo();
+    }
+}
