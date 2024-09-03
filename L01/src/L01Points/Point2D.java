a5bcbd29295f3dd8cce115b10f43854f625fc8e4
Dmitry <dmitry.wu@gmail.com>
2024-09-03 11:17:39 +0300

    add DRY principle sample

--- L01/src/L01Points/Point2D.java
+++ L01/src/L01Points/Point2D.java
@@ -8,15 +8,25 @@ public class Point2D {
         y = valueY;
     }
 
-    public Point2D() {
-        x = 0;
-        y = 0;
+    public Point2D(int value) {
+        this(value, value);
+        // this.x = value;
+        // this.y = value;
     }
 
-    public Point2D(int value) {
-        x = value;
-        y = value;
+    public Point2D() {
+        this(0);
     }
+
+    // public Point2D() {
+    //     x = 0;
+    //     y = 0;
+    // }
+
+    // public Point2D(int value) {
+    //     x = value;
+    //     y = value;
+    // }
 
     private String getInfo() {
         return String.format("x: %d; y: %d", x, y);
